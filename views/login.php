acc2ba9a513546318bc4207a7eec6a89eb9471aa
Hi Heba <hebaelhalawany2000@gmail.com>
2025-04-23 23:37:35 +0200

    login

---
+++ views/login.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="post">
+        <input type="text" name="name" placeholder="name" >
+        <input type="password" name="password" placeholder="password">
+        <input type="submit" value="Login">
+        <input type="">
+    </form>
+</body>
+</html>
