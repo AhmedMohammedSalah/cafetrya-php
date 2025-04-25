d2c29b1d0e58a6dfc8df2baf15b94323fab6d414
hp <omarokstar2001@gmail.com>
2025-04-25 19:26:34 +0300

    test

--- models/product.php
+++ models/product.php
@@ -1,15 +1,41 @@
-<?php
+<!-- <?php
+
+// // add product 
+// function addProduct($productName, $image ,$price, $categoryId) {
+//     include_once(__DIR__ . '/../connection.php');
+//     $sql = "INSERT INTO products (productName, image, price, categoryId) VALUES ('$productName', '$image', $price, $categoryId)";
+//     if (mysqli_query($myconnection, $sql)) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+    
+// }
+
+?>
+<?php 
 
 // add product 
 function addProduct($productName, $image ,$price, $categoryId) {
     include_once(__DIR__ . '/../connection.php');
-    $sql = "INSERT INTO products (productName, image, price, categoryId) VALUES ('$productName', '$image', $price, $categoryId)";
+    $sql = "INSERT INTO products (product_name, image, price, category_Id) VALUES ('$productName', '$image', $price, $categoryId)";
     if (mysqli_query($myconnection, $sql)) {
         return true;
     } else {
         return false;
     }
     
+}
+function getAllProducts() {
+    include_once(__DIR__ . '/../connection.php');
+    $sql = "select * from products";
+    $allProducts = mysqli_query($myconnection, $sql);
+
+    if (!$allProducts) {
+        die("Query failed: " . mysqli_error($myconnection));
+    }
+
+    return $allProducts;
 }
 
 ?>
---
+++ models/room.php
@@ -0,0 +1,18 @@
+<?php
+function getAllRooms() {
+    include_once(__DIR__ . '/../connection.php');
+
+    $sql = "SELECT * FROM rooms ORDER BY room_name ASC";
+    $result = mysqli_query($myconnection, $sql);
+
+    if (!$result) {
+        die("Query failed: " . mysqli_error($myconnection)); 
+    }
+
+    if (mysqli_num_rows($result) == 0) {
+        echo "No rooms found!";
+    }
+
+    return $result;
+}
+?>
