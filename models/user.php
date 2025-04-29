d1c89df72aa69f71ebae369efb3ad8241bea9ebd
ahmed <amhmdslah104@gmail.com>
2025-04-29 05:42:40 +0300

    check mail function

--- models/user.php
+++ models/user.php
@@ -6,7 +6,15 @@ function getUserById($userId) {
     $result = mysqli_query($myconnection, $sql);
     return mysqli_fetch_assoc($result);
 }
-
+function checkMail ($email) {
+    $sql = "SELECT * FROM users WHERE email = '$email'";
+    include(__DIR__ . '/../connection.php');
+    if (mysqli_query($myconnection, $sql)) {
+        return true;
+    } else {
+        return false;
+    }
+}
 function addUser($name, $password, $email,$image,$age , $room_id) {
     $sql = "INSERT INTO users (name, password, email, image, age, room_id) VALUES ('$name', '$password', '$email', '$image', $age, $room_id)";
     include(__DIR__ . '/../connection.php');
